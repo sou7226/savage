c686ec03a6542b81b7d970f5602745e07b451637
HashimotoKanna <63573845+HashimotoKanna@users.noreply.github.com>
2024-02-08 01:01:08 +0900

    refactor:

--- bots.js
+++ bots.js
@@ -36,163 +36,141 @@ let atkcounter = 0, time, targetChannelID;
 let atkmsg1 = "::atk", atkmsg2 = "::atk", atkmsg3 = "::atk", atkmsg4 = "::atk";
 
 async function UsedElixir(client, message, atkmsg, coolTime, usedElixirCoolTime) {
-    try {
-        if (
-            message.content.includes(`${client.user.username}のHP:`) &&
-            !message.content.includes(`${client.user.username}はやられてしまった。。。`) &&
-            !message.content.includes('を倒した！')
-        ) {
-            await timeout(coolTime)
-            message.channel.send(atkmsg)
-        } else if (
-            message.content.includes(`${client.user.username}のHP:`) &&
-            message.content.includes(`${client.user.username}はやられてしまった。。。`) &&
-            !message.content.includes('を倒した！') ||
-            message.content.includes(`${client.user.username}のHP:`) &&
-            message.content.includes(`${client.user.username}は自滅してしまった。。。`) &&
-            !message.content.includes('を倒した！') ||
-            message.content.includes(`<@${client.user.id}>はもうやられている！`)
-        ) {
-            await timeout(coolTime)
-            message?.channel.send(ResetSSRFlag && SSRFlag ? "::atk" : "::i e")
-            atkcounter++;
-            await timeout(usedElixirCoolTime)
-            message?.channel.send(SSRMessage(atkmsg))
-            atkcounter++;
-        }
-
-    } catch (err) {
-        console.error(err);
+    if (
+        message.content.includes(`${client.user.username}のHP:`) &&
+        !message.content.includes(`${client.user.username}はやられてしまった。。。`) &&
+        !message.content.includes('を倒した！')
+    ) {
+        await timeout(coolTime)
+        message.channel.send(atkmsg)
+    } else if (
+        message.content.includes(`${client.user.username}のHP:`) &&
+        message.content.includes(`${client.user.username}はやられてしまった。。。`) &&
+        !message.content.includes('を倒した！') ||
+        message.content.includes(`${client.user.username}のHP:`) &&
+        message.content.includes(`${client.user.username}は自滅してしまった。。。`) &&
+        !message.content.includes('を倒した！') ||
+        message.content.includes(`<@${client.user.id}>はもうやられている！`)
+    ) {
+        await timeout(coolTime)
+        message?.channel.send(ResetSSRFlag && SSRFlag ? "::atk" : "::i e")
+        atkcounter++;
+        await timeout(usedElixirCoolTime)
+        message?.channel.send(SSRMessage(atkmsg))
+        atkcounter++;
     }
-
 }
 function spawnSuperRareProcess() {
     message.channel.send(`<@&${process.env.ROLE_ID}>`)
     SSRFlag = true
 }
 client1.on("messageCreate", async (message) => {
-    try {
-        if (!adminId.has(message.author.id) && message.guild.id.includes(guildIds)) return;
-        [targetChannelID, ResetSSRFlag, duoFlag] = await functions.setChannel(prefixes, message, targetChannelID, ResetSSRFlag, atkmsg1, duoFlag) //client1でしか操作不可
-        if (message.content.startsWith(prefix1)) {
-            atkmsg1 = await functions.moderate(client1, message, prefix1, atkmsg1)
+    if (!adminId.has(message.author.id) && message.guild.id.includes(guildIds)) return;
+    [targetChannelID, ResetSSRFlag, duoFlag] = await functions.setChannel(prefixes, message, targetChannelID, ResetSSRFlag, atkmsg1, duoFlag) //client1でしか操作不可
+    if (message.content.startsWith(prefix1)) {
+        atkmsg1 = await functions.moderate(client1, message, prefix1, atkmsg1)
+    }
+    if (targetChannelID !== message.channel.id) return;
+    if (message.embeds.length > 0 && message.embeds[0].title) {
+        const embedTitle = message.embeds[0].title;
+        if (embedTitle.includes("が待ち構えている")) {
+            SSRFlag = false
+            if (message.embeds[0].author.name.includes("超激レア")) {
+                spawnSuperRareProcess()
+            }
+            if (message.embeds[0].author.name &&
+                message.embeds[0].author.name.includes("超強敵") ||
+                embedTitle.includes("ジャックフロスト")) {
+                atkFlag = atkmsg1
+                atkmsg1 = "::i f"
+            }
+            await timeout(coolTime)
+            message.channel.send(atkmsg1)
+        } else if (embedTitle.includes("戦闘結果")) {
+            atkcounter = 0;
+            atkmsg1 = atkFlag
         }
-        if (targetChannelID !== message.channel.id) return;
-        if (message.embeds.length > 0 && message.embeds[0].title) {
-            const embedTitle = message.embeds[0].title;
-            if (embedTitle.includes("が待ち構えている")) {
-                SSRFlag = false
-                if (message.embeds[0].author.name.includes("超激レア")) {
-                    spawnSuperRareProcess()
-                }
-                if (message.embeds[0].author.name &&
-                    message.embeds[0].author.name.includes("超強敵") ||
-                    embedTitle.includes("ジャックフロスト")) {
-                    atkFlag = atkmsg1
-                    atkmsg1 = "::i f"
-                }
+    } else {
+        if (duoFlag && isKeepFighting(client2, message)) {
+            await timeout(coolTime)
+            message.channel.send(atkmsg1)
+        }
+        if (atkmsg4 === "::atk") {
+            if (isKeepFighting(client4, message)) {
                 await timeout(coolTime)
                 message.channel.send(atkmsg1)
-            } else if (embedTitle.includes("戦闘結果")) {
-                atkcounter = 0;
-                atkmsg1 = atkFlag
             }
-        } else {
-            if (duoFlag && isKeepFighting(client2, message)) {
+        } else if (atkmsg4 === "::i f") {
+            if (isFightFb(client4, message)) {
                 await timeout(coolTime)
-                message.channel.send(atkmsg1)
-            }
-            if (atkmsg4 === "::atk") {
-                if (isKeepFighting(client4, message)) {
-                    await timeout(coolTime)
-                    message.channel.send(atkmsg1)
-                }
-            } else if (atkmsg4 === "::i f") {
-                if (isFightFb(client4, message)) {
-                    await timeout(coolTime)
-                    message?.channel.send(atkmsg1)
-                }
+                message?.channel.send(atkmsg1)
             }
         }
-    } catch (err) {
-        console.error(err);
     }
 });
 
 client2.on("messageCreate", async (message) => {
-    try {
-        if (!adminId.has(message.author.id) && message.guild.id.includes(guildIds)) return;
-        if (message.content.startsWith(prefix2)) {
-            atkmsg2 = await functions.moderate(client2, message, prefix2, atkmsg2)
-        }
-        if (targetChannelID !== message.channel.id) return;
-        clearTimeout(time);
-        if (atkmsg1 === "::atk") {
-            if (duoFlag) {
-                await UsedElixir(client1, message, atkmsg2, coolTime, usedElixirCoolTime)
-            } else if (isKeepFighting(client1, message)) {
-                await timeout(coolTime)
-                message.channel.send(SSRMessage(atkmsg2))
-                atkcounter++;
-            }
-        } else if (atkmsg1 === "::i f") {
-            if (isFightFb(client1, message)) {
-                await timeout(coolTime)
-                message?.channel.send(SSRMessage(atkmsg2))
-                atkcounter++;
-            }
-        }
-        time = setTimeout(() => message.channel?.send(SSRMessage(atkmsg2) + " to"), Timeout)
-    } catch (err) {
-        console.error(err);
+    if (!adminId.has(message.author.id) && message.guild.id.includes(guildIds)) return;
+    if (message.content.startsWith(prefix2)) {
+        atkmsg2 = await functions.moderate(client2, message, prefix2, atkmsg2)
     }
+    if (targetChannelID !== message.channel.id) return;
+    clearTimeout(time);
+    if (atkmsg1 === "::atk") {
+        if (duoFlag) {
+            await UsedElixir(client1, message, atkmsg2, coolTime, usedElixirCoolTime)
+        } else if (isKeepFighting(client1, message)) {
+            await timeout(coolTime)
+            message.channel.send(SSRMessage(atkmsg2))
+            atkcounter++;
+        }
+    } else if (atkmsg1 === "::i f") {
+        if (isFightFb(client1, message)) {
+            await timeout(coolTime)
+            message?.channel.send(SSRMessage(atkmsg2))
+            atkcounter++;
+        }
+    }
+    time = setTimeout(() => message.channel?.send(SSRMessage(atkmsg2) + " to"), Timeout)
 });
 
 client3.on("messageCreate", async (message) => {
-    try {
-        if (!adminId.has(message.author.id) && message.guild.id.includes(guildIds)) return;
-        if (message.content.startsWith(prefix3)) {
-            atkmsg3 = await functions.moderate(client3, message, prefix3, atkmsg3)
-        }
-        if (targetChannelID !== message.channel.id) return;
-        if (!SSRFlag || !ResetSSRFlag) {
-            if (atkmsg2 === "::atk") {
-                if (duoFlag) return;
-                if (
-                    isKeepFighting(client2, message)
-                ) {
-                    await timeout(coolTime)
-                    message.channel.send(atkmsg3)
-                }
-            } else if (atkmsg2 === "::i f") {
-                if (isFightFb(client2, message)) {
-                    await timeout(coolTime)
-                    message?.channel.send(atkmsg3)
-                }
+    if (!adminId.has(message.author.id) && message.guild.id.includes(guildIds)) return;
+    if (message.content.startsWith(prefix3)) {
+        atkmsg3 = await functions.moderate(client3, message, prefix3, atkmsg3)
+    }
+    if (targetChannelID !== message.channel.id) return;
+    if (!SSRFlag || !ResetSSRFlag) {
+        if (atkmsg2 === "::atk") {
+            if (duoFlag) return;
+            if (
+                isKeepFighting(client2, message)
+            ) {
+                await timeout(coolTime)
+                message.channel.send(atkmsg3)
+            }
+        } else if (atkmsg2 === "::i f") {
+            if (isFightFb(client2, message)) {
+                await timeout(coolTime)
+                message?.channel.send(atkmsg3)
             }
         }
-    } catch (err) {
-        console.error(err);
     }
 });
 
 client4.on("messageCreate", async (message) => {
-    try {
-        if (!adminId.has(message.author.id) && message.guild.id.includes(guildIds)) return;
-        if (message.content.startsWith(prefix4)) {
-            atkmsg4 = await functions.moderate(client4, message, prefix4, atkmsg4)
+    if (!adminId.has(message.author.id) && message.guild.id.includes(guildIds)) return;
+    if (message.content.startsWith(prefix4)) {
+        atkmsg4 = await functions.moderate(client4, message, prefix4, atkmsg4)
+    }
+    if (targetChannelID !== message.channel.id) return;
+    if (atkmsg3 === "::atk") {
+        await UsedElixir(client3, message, atkmsg4, coolTime, usedElixirCoolTime)
+    } else if (atkmsg3 === "::i f") {
+        if (isFightFb(client3, message)) {
+            await timeout(coolTime)
+            message?.channel.send(atkmsg4)
         }
-        if (targetChannelID !== message.channel.id) return;
-        if (atkmsg3 === "::atk") {
-            await UsedElixir(client3, message, atkmsg4, coolTime, usedElixirCoolTime)
-        } else if (atkmsg3 === "::i f") {
-            if (isFightFb(client3, message)) {
-                await timeout(coolTime)
-                message?.channel.send(atkmsg4)
-            }
-        }
-    } catch (err) {
-        console.error(err);
     }
 });
 
